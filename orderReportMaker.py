d08b4e3a4cd8103231786def285c3f5a11fa02f3
Carrie-Scott <carrie.scott@ibm.com@carriescomputer.home>
2020-04-26 12:10:58 -0500

    revixed py file

--- orderReportMaker.py
+++ orderReportMaker.py
@@ -9,6 +9,7 @@ import pandas as pd
 import numpy as np
 from pandas import ExcelFile
 from pandas import ExcelWriter
+import openpyxl
 
 # read in full export
 export = pd.read_csv(f'{download_folder}orders_export.csv', dtype={'Phone':str })
@@ -22,57 +23,76 @@ now = datetime.now()
 today = datetime(now.year, now.month, now.day)
 today_string = today.strftime("%Y-%m-%d")
 
-#Filter based on day of the week
-  #if it's Monday, then get the past 2 days of orders
-if today.weekday() ==0:
-  sunday = export[export['Date Ordered'] == today - timedelta(days=1)]
-  saturday = export[export['Date Ordered'] == today - timedelta(days=2)]
-  orders = pd.concat([sunday, saturday], ignore_index = True)
-else:
-  orders = export[export['Date Ordered'] == today - timedelta(days=1)]
+orders = export
+# filter out orders from today
+orders = orders[orders['Date Ordered'] != today]
+# orders = orders[orders['Date Ordered'] == today]
 
-# Fill in missing values from row above to make up for prob with shopify export
-orders = orders.fillna(method='ffill')
+# Fill in missing values for SOME columns with info from row above
+orders['Fulfillment Status'] = orders['Fulfillment Status'].fillna(method='ffill')
+orders['Shipping Name'] = orders['Shipping Name'].fillna(method='ffill')
+orders['Billing Name'] = orders['Billing Name'].fillna(method='ffill')
+orders['Shipping Street'] = orders['Shipping Street'].fillna(method='ffill')
+orders['Shipping City'] = orders['Shipping City'].fillna(method='ffill')
+orders['Shipping Province'] = orders['Shipping Province'].fillna(method='ffill')
+orders['Shipping Zip'] = orders['Shipping Zip'].fillna(method='ffill')
+orders['Shipping Country'] = orders['Shipping Country'].fillna(method='ffill')
+orders['Shipping Method'] = orders['Shipping Method'].fillna(method='ffill')
+orders['Financial Status'] = orders['Financial Status'].fillna(method='ffill')
 
 # Filter out pre-orders
-orders = orders[~orders['Lineitem name'].str.contains('pre-order')]
+#orders = orders[~orders['Lineitem name'].str.contains('pre-order')]
 
 # Filter out fulfilled
-orders = orders[orders['Fulfillment Status'].str.contains('unfulfilled')]
+orders = orders[~orders['Fulfillment Status'].str.match('fulfilled')]
+
+# Filter to only get 'paid' (to get rid of refunded)
+orders = orders[~orders['Financial Status'].str.match('refunded')]
 
 # Shorten description for instore-pickup
 orders.loc[orders['Shipping Method'].str.contains('in-store'),['Shipping Method']]= 'in-store pickup'
 
-# Create dataframe for cutters
+# Shipping Method Summary
+summary = orders.copy()
+summary.loc[summary['Shipping Method'].str.contains('Delivery|Austin|shipping'), 'Shipping Method'] = 'Delivery'
+summary.loc[summary['Shipping Method'].str.contains('pickup'), 'Shipping Method'] = "Pick Up"
+summary = summary[['Lineitem name','Name', 'Shipping Method', 'Lineitem quantity']]
+summary = pd.pivot_table(summary, index=['Shipping Method', 'Lineitem name'], values='Lineitem quantity', aggfunc=np.sum)
+
+# Create list for cutters
 to_make = orders.copy()
-to_make.loc[to_make['Shipping Method'].str.contains('Delivery|in-store'), 'Shipping Method'] = 'Delivery/in-store'
-to_make = to_make[['Lineitem name','Name', 'Shipping Method']]
-to_make = pd.pivot_table(to_make, index=['Lineitem name', 'Shipping Method'], aggfunc='count')
+to_make = pd.pivot_table(to_make, index=['Lineitem name'], values='Lineitem quantity', aggfunc=np.sum)
 
 # Create dataframe for instore pickups
-pick_ups = orders[orders['Shipping Method'].str.contains('in-store')][['Date Ordered',
-                                                                      'Billing Name',
-                                                                      'Lineitem name',
-                                                                      'Shipping Method',
-                                                                      'Email',
-                                                                      'Phone' ]]
+pick_ups = orders[orders['Shipping Method'].str.contains('in-store')][['Name',
+                                                                       'Date Ordered',
+                                                                       'Shipping Name',
+                                                                       'Lineitem name',
+                                                                       'Lineitem quantity',
+                                                                       'Shipping Method',
+                                                                       'Email',
+                                                                       'Phone']]
 
 # Create dataframe for deliveries
-deliveries = orders[orders['Shipping Method'].str.contains('Delivery')][['Date Ordered',
+deliveries = orders[orders['Shipping Method'].str.contains('Delivery')][['Name',
+                                                            'Date Ordered',
                                                             'Lineitem name',
+                                                            'Lineitem quantity',
                                                             'Shipping Method',
-                                                            'Billing Name', 
+                                                            'Shipping Name', 
                                                             'Shipping Street',
                                                             'Shipping City', 
                                                             'Shipping Zip',
                                                             'Shipping Phone', 
-                                                            'Notes',]].sort_values(by=['Shipping Zip'])
+                                                            'Notes',]].sort_values(by=['Shipping Method', 'Name'])
 
 # Create dataframe for shipping
-to_ship = orders[orders['Shipping Method'].str.contains('UPS')][['Date Ordered',
+to_ship = orders[orders['Shipping Method'].str.contains('UPS')][['Name',
+                                                            'Date Ordered',
                                                             'Lineitem name',
+                                                            'Lineitem quantity',
                                                             'Shipping Method',
-                                                            'Billing Name',
+                                                            'Shipping Name',
                                                             'Shipping Name',
                                                             'Shipping Street',
                                                             'Shipping City', 
@@ -82,7 +102,12 @@ to_ship = orders[orders['Shipping Method'].str.contains('UPS')][['Date Ordered',
 
 # Print all dataframes to excel
 with pd.ExcelWriter(f'{download_folder}Online Order Reports-{today_string}.xlsx') as writer:
-    to_make.to_excel(writer, sheet_name='To Make', index=True)
+    summary.to_excel(writer, sheet_name='Method Summary')
+    to_make.to_excel(writer, sheet_name='To Make')
     pick_ups.to_excel(writer, sheet_name='Pick Ups', index=False)
     deliveries.to_excel(writer,sheet_name='Deliveries', index=False)
     to_ship.to_excel(writer, sheet_name='To Ship', index=False)
+
+# print unique delivery number
+delNum=deliveries['Shipping Name'].nunique()
+print(f'Unique Deliveries: {delNum}')                                                                                                                       
